894bb8904a61c7c80253dcbc3b2d4cc60ba5e970
aleks <aleks.zakatov@gmail.com>
2018-02-21 21:35:38 +0200

    unit_2 Module 5 - 2.5.1

---
+++ app/code/Training/Test/Controller/Action/Index.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Training\Test\Controller\Action;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute() {
+        $this->getResponse()->appendBody("HELLO WORLD");
+    }
+}
